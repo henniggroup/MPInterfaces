acc2096907faf827a8658055b90ce85b2410d2ba
Kiran Mathew <km468@cornell.edu>
2014-12-05 21:49:22 -0500

    rewrote vasp_preprocess.py script to use the instrument module

--- vasp_preprocess.py
+++ vasp_preprocess.py
@@ -1,9 +1,10 @@
 import os
 import sys
 import shutil as shu
-from pymatgen.io.vaspio.vasp_input import Incar, Potcar, Kpoints
+from pymatgen.io.vaspio.vasp_input import Incar, Poscar, Potcar, Kpoints
 from pymatgen.core.structure import Structure
 import subprocess as sp
+from instrument import CalibrateMolecule
 
 #dictionary of defaults for molecule ENCUT convergence
 default_incar = {'SYMPREC': 1e-06, 
@@ -21,27 +22,32 @@ default_incar = {'SYMPREC': 1e-06,
                  'PREC': 'Accurate'}
 
 incar = Incar.from_dict(default_incar)
-poscar = Structure.from_file("POSCAR", sort=True)
-symbols = poscar.symbol_set
+poscar = Poscar.from_file('POSCAR', check_for_POTCAR=False)#Structure.from_file("POSCAR", sort=True)
+symbols = poscar.site_symbols #symbol_set
 potcar = Potcar(symbols)
 kpoints = Kpoints()
 
+calmol = CalibrateMolecule(incar, poscar, potcar, kpoints)
+calmol.encut_cnvg(range(400,800,100))
+calmol.run()
+
+
 #create INCAR loop according to ENCUT
 #assign ENCUT value
-for E in range(400,850,50):
-	sp.check_call("rm -rf "+str(E), shell = True)	
-	os.mkdir(str(E))
-	os.chdir(str(E))
-	incar['ENCUT'] = E
-	incar.write_file("INCAR")
-	poscar.write_file("POSCAR")
-	potcar.write_file("POTCAR")
-	kpoints.write_file("KPOINTS")
-        shu.copy("../submit_job",".")
-	ret_val = sp.check_call("qsub submit_job", shell=True)
-        if ret_val != 0:
-                sys.exit()
-	os.chdir('..')
+#for E in range(400,850,50):
+#	sp.check_call("rm -rf "+str(E), shell = True)	
+#	os.mkdir(str(E))
+#	os.chdir(str(E))
+#	incar['ENCUT'] = E
+#	incar.write_file("INCAR")
+#	poscar.write_file("POSCAR")
+#	potcar.write_file("POTCAR")
+#	kpoints.write_file("KPOINTS")
+#        shu.copy("../submit_job",".")
+#	ret_val = sp.check_call("qsub submit_job", shell=True)
+#        if ret_val != 0:
+#                sys.exit()
+#	os.chdir('..')
 
 
 

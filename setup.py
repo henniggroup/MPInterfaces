9b251bc9b7dda8dc3ef1c92d852d9685c3039308
Kiran Mathew <km468@cornell.edu>
2014-12-07 16:26:22 -0500

    fixed setup.py

--- setup.py
+++ setup.py
@@ -12,15 +12,21 @@ setup(
     version = "0.0.0",
     author = "Kiran Mathew, Joshua Gabriel",
     author_email = "km468@cornell.edu",
-    description = ("vasp automation"),
+    description = ("Automate vasp calculations for interfaces"),
     license = "GPL",
     url = "https://matk86@bitbucket.org/matk86/vasp_automation.git",
     packages=find_packages(),
     long_description=read('README'),
     classifiers=[
+        "Programming Language :: Python :: 2.7",
         "Development Status :: 3 - Alpha",
-        "Topic :: Utilities",
+        "Intended Audience :: Science/Research",
         "License :: OSI Approved :: GPL License",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Information Analysis",
+        "Topic :: Scientific/Engineering :: Physics",
+        "Topic :: Scientific/Engineering :: Chemistry",
+        "Topic :: Software Development :: Libraries :: Python Modules",
     ],
     scripts=glob.glob(os.path.join(SETUP_PTH, "scripts", "*"))
 )

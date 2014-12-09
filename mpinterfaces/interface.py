fd50a6989ff63af9414aa2880e06b008511d15ed
Kiran Mathew <km468@cornell.edu>
2014-12-08 22:22:00 -0500

    temporary

--- mpinterfaces/interface.py
+++ mpinterfaces/interface.py
@@ -10,7 +10,7 @@ class Interface(Slab):
     Interface = slab + ligand + environment(solvent)
     """
     def __init__(self, strt, hkl=[1,1,1], min_thick=10, min_vac=10, supercell=[1,1,1],
-                 ligand=None, displacement=1.0, solvent=None, start_from_slab=False,
+                 ligand=None, displacement=1.0, sface_cvrg=None, solvent=None, start_from_slab=False,
                  validate_proximity=False, to_unit_cell=False, coords_are_cartesian=False):
         self.strt = strt
         self.hkl = hkl
@@ -21,6 +21,7 @@ class Interface(Slab):
         self.displacement = displacement
         self.solvent = solvent
         self.start_from_slab = start_from_slab
+        self.sface_cvrg = sface_cvrg
         
         Slab.__init__(self, self.strt.lattice, self.strt.species_and_occu, self.strt.frac_coords, miller_index=self.hkl, oriented_unit_cell=None, shift=None, scale_factor=None, validate_proximity=validate_proximity,
                            to_unit_cell=to_unit_cell, coords_are_cartesian=coords_are_cartesian,
@@ -31,28 +32,70 @@ class Interface(Slab):
         get all the top atom coords
         """
         cart_coords = self.strt.cart_coords
-        max_dist = np.max(self.strt.distance_matrix[0,:])
+        n_atoms = len(cart_coords[:,0])
+        max_dist = np.max(self.strt.distance_matrix.reshape(n_atoms*n_atoms, 1))
         self.top_atoms = []
-        for j in range(len(cart_coords[:,0])):
+        for j in range(n_atoms):
             if j not in self.top_atoms:
-                [self.top_atoms.append(i) for i in range(len(cart_coords[:,0])) if (max_dist - self.strt.distance_matrix[j,i]) < 1e-6]
+                [self.top_atoms.append(i) for i in range(n_atoms) if np.abs(max_dist - self.strt.distance_matrix[j,i]) < 1e-6]
+        self.top_atoms = np.unique(self.top_atoms)
 
+    def enforce_surface_cvrg(self):
+        m = self.strt.lattice.matrix
+        surface_area = np.linalg.norm(np.cross(m[0], m[1]))
+        #n_top_atoms =  len(self.top_atoms)
+        #print 'number of top atoms ', n_top_atoms
+        if self.sface_cvrg:
+           for i in range(1, 10):
+                for j in range(1, 10):
+                    #for nlig in range(1, i*j*n_top_atoms):
+                        
+                        surface_area = np.linalg.norm(np.cross(i*m[0], j*m[1]))
+                        if (int(surface_area * self.sface_cvrg) == 1):
+                                print 'supercell = ', i, j
+                                print 'feasible covergae = ', 1./surface_area, ' requested = ', self.sface_cvrg
+                                self.strt.make_supercell([i,j,1])
+                                return
+
+
+    def adsorb_on(self, site_indices):
+        mov_vec = self.strt.normal * self.displacement
+#        for i in range(len(self.strt.cart_coords)):
+#            if np.linalg.norm(self.strt.cart_coords[i] - mov_vec) <= 3.0:
+#                mov_vec = -mov_vec
+#                break
+        #print self.strt.cart_coords[site_indices[0]]
+        #sys.exit()
+        
+        adsorbed_ligands_coords = np.array([self.ligand.cart_coords - self.strt.cart_coords[sindex] + mov_vec for sindex in site_indices ]) #3d numpy array
+        #extend the slab structure with the adsorbant atoms
+        num_atoms = len(self.ligand.species_and_occu)
+        for j in range(len(site_indices)):
+            [self.strt.append(self.ligand.species_and_occu[i], adsorbed_ligands_coords[j,i,:], coords_are_cartesian=True) for i in range(num_atoms)]
+        
                 
     def create_interface(self):
         if not self.start_from_slab:
             self.strt = SlabGenerator(self.strt, self.hkl, self.min_thick, self.min_vac).get_slab()
         self.strt.to(fmt='poscar', filename='POSCAR_primitive_slab.vasp')
-        #create supercell of the slab
-        self.strt.make_supercell(supercell)
-        self.strt.to(fmt='poscar', filename='POSCAR_slab_supercell.vasp')
-        normal =  self.strt.normal
+        #self.strt.to(fmt='poscar', filename='POSCAR_slab_supercell.vasp')
+        #self.set_top_atoms()
+        #self.adsorb_sites = self.top_atoms
+        print self.strt
+        self.enforce_surface_cvrg()
+        print self.strt
         self.set_top_atoms()
-        #set adatoms_coords wrt the top atoms in Angstrom
-        adsorbed_ligands_coords = np.array([self.ligand.cart_coords + self.strt.cart_coords[top_atom] + normal * self.displacement for top_atom in self.top_atoms ]) #3d numpy array
-        #extend the slab structure with the adsorbant atoms
-        num_atoms = len(self.ligand.species_and_occu)
-        for j in range(len(self.top_atoms)):
-            [self.strt.append(self.ligand.species_and_occu[i], adsorbed_ligands_coords[j,i,:], coords_are_cartesian=True) for i in range(num_atoms)]
+        self.adsorb_sites = self.top_atoms
+        print self.adsorb_sites
+        sys.exit()
+        #adsorb one ligand on one of the top atoms
+        self.adsorb_on([self.adsorb_sites[0]])        
+        #sys.exit()
+        #print 'surface coverage ', 1./self.strt.surface_area
+
+
+        #create supercell of the slab to enforce the given surface coverage
+        self.enforce_surface_cvrg()
 
 
     @staticmethod
@@ -301,7 +344,7 @@ if __name__=='__main__':
     #adsorb a molecule(shifted by shift from the slab top surface) on
     #NOTE: the molecule is adsorbed on all atoms of the top surface of the slab
     #TODO: add surface coverage(ligands per nm^2)
-    iface = Interface(strt, hkl=hkl, min_thick=min_thick, min_vac=min_vac, supercell=supercell, ligand=h2o, displacement=displacement)
+    iface = Interface(strt, hkl=hkl, min_thick=min_thick, min_vac=min_vac, supercell=supercell, ligand=h2o, displacement=displacement, sface_cvrg=0.1) # 1 lig/nm^2 = 0.01 lig/ang^2
     iface.create_interface()
     iface.write_to_file('poscar', 'POSCAR_interface.vasp')
 

f3582838fc2a137e1ae640c6ce0cf315f801c6a0
Kiran Mathew <km468@cornell.edu>
2014-12-19 09:49:31 -0500

    bug fix in firetasks

--- mpinterfaces/firetasks.py
+++ mpinterfaces/firetasks.py
@@ -96,10 +96,10 @@ class MPINTMeasurementTask(FireTaskBase, FWSerializable):
                 obj.knob_settings(rootpath=obj.job_dir)
         if measure.calslab:
             for obj in measure.calslab:
-                obj.knob_settings(rootpath=obj.job_dir))
+                obj.knob_settings(rootpath=obj.job_dir)
         if measure.calbulk:
             for obj in measure.calbulk:
-                obj.knob_settings(rootpath=obj.job_dir))
+                obj.knob_settings(rootpath=obj.job_dir)
                 
                 
 

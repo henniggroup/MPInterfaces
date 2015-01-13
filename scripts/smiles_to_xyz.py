8b41e0c703af88c4eca7524ae8dbcff8abd557ad
Kiran Mathew <km468@cornell.edu>
2015-01-13 10:40:55 -0500

    added script to convert smiles to xyz using openbabel

---
+++ scripts/smiles_to_xyz.py
@@ -0,0 +1,15 @@
+"""
+uses openbabel python api to convert a smiles string represntation of
+a molecule to its xyz representation
+
+openbabel with python extensions must be made available
+"""
+
+import openbabel
+import pybel
+from pybel import readstring
+
+smi_input = "C1=CC=CS1"
+mol = readstring("smi", smi_input) 
+mol.make3D()
+mol.write(format="xyz", filename='out.xyz', overwrite=True)

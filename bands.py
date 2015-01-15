e08b23e84d82a8829f975f6907e990fc9f9e2e60
Kiran Mathew <km468@cornell.edu>
2015-01-15 09:18:10 -0500

    added scripts for plotting the bandstructure and the density of states

---
+++ bands.py
@@ -0,0 +1,16 @@
+"""
+reads in KPOINTS(with labels) and vasprun.xml files and plots the band structure
+"""
+
+from pymatgen.io.vaspio.vasp_output import Vasprun
+from pymatgen.electronic_structure.plotter import BSPlotterProjected, BSPlotter 
+
+if __name__ == "__main__":
+    # readin bandstructure from vasprun.xml and labeled KPOINTS
+    run = Vasprun("vasprun.xml", parse_projected_eigen = True)
+    bands = run.get_band_structure("KPOINTS", line_mode = True, efermi = run.efermi)
+    bsp =  BSPlotter(bands)
+    #Blue lines are up spin, red lines are down spin
+    bsp.save_plot('band_diagram.eps', ylim=(-5,5))
+    #bsp = BSPlotterProjected(bands)
+    #plt = bsp.get_projected_plots_dots( {'Fe':['s', 'p', 'd'],'Sb':['s', 'p', 'd']}) #get_elt_projected_plots_color()

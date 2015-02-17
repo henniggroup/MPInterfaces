967a26e7af8768176ef1b162bc41bddaf029de01
Kiran Mathew <km468@cornell.edu>
2015-02-17 11:41:27 -0500

    added test_workflow. changed the default vasp bin paths in firetasks

--- mpinterfaces/firetasks.py
+++ mpinterfaces/firetasks.py
@@ -46,7 +46,7 @@ def get_qadapter(nnodes=1, nprocs=16, walltime='24:00:00',
     #hipergator
     if 'ufhpc' in socket.gethostname():
         if job_bin is None:
-            job_bin='/home/km468/Software/VASP/vaspsol_kappa.5.3.5/vasp'
+            job_bin='/home/km468/Software/VASP/vasp.5.3.5/vasp'
         else:
             job_bin = job_bin
         d = {'type':'PBS',
@@ -63,7 +63,7 @@ def get_qadapter(nnodes=1, nprocs=16, walltime='24:00:00',
     #stampede
     elif 'stampede' in socket.gethostname():
         if job_bin is None:
-            job_bin='/home1/01682/km468/Software/VASP/vaspsol_kappa.5.3.5/vasp'
+            job_bin='/home1/01682/km468/Software/VASP/vasp.5.3.5/vasp'
         else:
             job_bin = job_bin
         d = {'type':'SLURM',
@@ -107,7 +107,7 @@ def get_cal_obj(d):
                                                       'carbon']:
         job_cmd = ['nohup', '/opt/openmpi_intel/bin/mpirun',
                    '-n', '24',
-                   '/home/km468/Software/VASP_JDFTx/VASP/vasp_kappa.5.3.5/vasp']
+                   '/home/km468/Software/VASP/vasp.5.3.5/vasp']
         if d.get("other_params") is not None:
             d.get("other_params").update({'wait':False})
         else:
---
+++ scripts/workflow_examples/test_workflow.py
@@ -0,0 +1,70 @@
+from __future__ import division, unicode_literals, print_function
+
+"""
+This is a simpler verison of single_workflow with just one firework
+and firetask.
+used only for testing.
+Shows how to set a firework's launch directory
+"""
+
+import sys
+
+import numpy as np
+
+from pymatgen import Lattice
+from pymatgen.core.structure import Structure
+from pymatgen.io.vaspio.vasp_input import Incar, Poscar, Potcar, Kpoints
+
+from fireworks import Firework, Workflow, LaunchPad
+from fireworks.core.rocket_launcher import launch_rocket
+
+from mpinterfaces.firetasks import MPINTCalibrateTask, MPINTMeasurementTask
+
+a0 = 3.965
+lattice_matrix = np.array([ [0.5, 0.0, 0.5], [0.5, 0.5, 0.0], [0.0, 0.5, 0.5] ]) * a0
+lattice = Lattice(lattice_matrix)
+structure = Structure( lattice, ['Pt'], [ [0.0, 0.0, 0.0] ],
+                       coords_are_cartesian=False)
+incarparams = {'System':'test',
+                   'ENCUT': 400,
+                   'ISMEAR': 1,
+                   'SIGMA': 0.1,
+                   'EDIFF':1E-6}
+incar = Incar(params=incarparams)
+poscar = Poscar(structure, comment='test', selective_dynamics=None)
+potcar = Potcar(symbols = poscar.site_symbols, functional='PBE',
+                sym_potcar_map=None)
+kpoints = Kpoints(kpts=((8, 8, 8),))
+
+calparams1 = {}
+calparams1['incar'] = incar.as_dict()
+calparams1['poscar'] = poscar.as_dict()
+calparams1['kpoints'] = kpoints.as_dict()
+calparams1['que_params'] = { 'nnodes':1, 
+                             'nprocs':16, 
+                             'walltime':'24:00:00',
+                             'job_bin':'/home/km468/Software/VASP/vasp.5.3.5/vasp'
+                             }
+turn_knobs = { 'ENCUT' : range(400, 500, 100),
+               'KPOINTS': [k for k in range(20, 30, 10)]
+             }
+calparams1['calibrate'] = 'CalibrateBulk'
+calparams1['turn_knobs'] = turn_knobs
+calparams1['other_params'] = { 'job_dir':'calBulk'}
+
+caltask1 = MPINTCalibrateTask(calparams1)
+
+#firework with launch directory set to $FW_JOB_DIR, an environment variable
+fw_calibrate = Firework([caltask1], spec={'_launch_dir':'$FW_JOB_DIR'}, name="fw_test")
+wf = Workflow([fw_calibrate], name="mpint_wf_test")
+
+lp = LaunchPad(host='localhost', port=27017, name='fireworks',
+                          username="km468", password="km468")
+
+print('fireworks in the database before adding the workflow: \n',
+      lp.get_fw_ids())
+
+lp.add_wf(wf)
+
+print('fireworks in the database: \n', lp.get_fw_ids())
+

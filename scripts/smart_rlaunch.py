abb11a39a951486f5ca034f36f8d7ce044957d08
Kiran Mathew <km468@cornell.edu>
2015-02-20 07:48:10 -0500

    updated and tested smart_rlaunch on hipergator

--- scripts/smart_rlaunch.py
+++ scripts/smart_rlaunch.py
@@ -15,6 +15,7 @@ from fabric.api import settings, run
 #MAILDIR = '/home/km468/.fireworks/mail/hipergator/'
 MAILDIR = '/home/matk/Documents/mail/hipergator/'
 
+
 def check_done(job_id):
     for fname in os.listdir(MAILDIR):
         #print fname, os.path.getmtime(MAILDIR+fname) - time.time()
@@ -22,6 +23,7 @@ def check_done(job_id):
             print('id in mail {0} = {1}'.format(fname, job_id))            
             return True
 
+        
 def present_id(job_id, fname, search_string=b"Execution terminated"):
     with open(fname, 'r') as f:
         fm = mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ)
@@ -34,11 +36,20 @@ def present_id(job_id, fname, search_string=b"Execution terminated"):
         else:
             return False
 
+        
 def launch():
     descr  = "Smart rocket launcher."\
         "Uses the execution termination emails send by the batch system to "\
-        "launch fireworks that depend on other fireworks."\
-        "Also takes range of firework ids to launch"
+        "launch fireworks that depend on other fireworks i.e fireworks that "\
+        "have 'cal_objs' in their spec."\
+        "Takes in fw_ids of fireworks to be launched. "\
+        "Range specification of fw_ids is also supported."\
+        "All the jobs are launched to hipergator remotely using fabric."\
+        "Note: Ensure that the submit scripts have the appropriate email settings and "\
+        "that the mail daemon is fetching emails to the folder polled by this script."\
+        "Note: All rocket launches take place in the home directory. If "\
+        "_launch_dir spec is set for the firework the job files will be written to "\
+        "that folder and jobs to the batch system will be done from there."
     parser = ArgumentParser(description=descr)
     parser.add_argument('-f', '--fw_ids', help='one or more of fw_ids to run', 
                         default=None, type=int, nargs='+')
@@ -51,6 +62,7 @@ def launch():
         fw_ids += range(args.r_fw_ids[0], args.r_fw_ids[1])
     job_ids = None
     lp = LaunchPad.from_file(LAUNCHPAD_LOC)
+    print(fw_ids)
     if fw_ids is None:
         print('now fw ids given')
         return
@@ -58,29 +70,31 @@ def launch():
         m_fw = lp._get_a_fw_to_run(fw_id=fid, checkout=False)
         if m_fw is None:
             print('no firework with that id')
+            return
         fw_spec = dict(m_fw.spec)
         done = []
         if fw_spec.get('cal_objs',None) is not None:
             for calparams in fw_spec['cal_objs']:
                 if calparams.get('job_ids', None) is not None:
                     job_ids = calparams.get('job_ids', None)
-                    print('job ids : ',job_ids)
+                    print('depends on jobs with ids : ',job_ids)
                     if job_ids is not None:
                         for jid in job_ids:
                             done.append(check_done(jid))
                     else:
                         print('job_ids not set')
-                else:
-                    print('this firework doesnt have any cal_objs')
-                    done.append(True)
-            if done and all(done):
-                with settings(host_string='km468@hipergator.rc.ufl.edu'):
-                    run("ssh dev1 rlaunch singleshot -f "+str(fid))
-            else:
-                print("Haven't recieved execution termination confirmation from the jobs in the firework")
+        else:
+            print('this firework doesnt have any cal_objs')
+            done.append(True)
+        if done and all(done):
+            with settings(host_string='km468@hipergator.rc.ufl.edu'):
+                run("ssh dev1 rlaunch singleshot -f "+str(fid))
+        else:
+            print("Haven't recieved execution termination confirmation for the jobs in the firework from hipergator resource manager")
         time.sleep(3)
     return
-        
+
+
 if __name__ == '__main__':
     launch()
     ##test

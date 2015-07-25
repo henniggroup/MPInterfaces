d1647f0b6552fa5d85df6a7508c0609cbb1c176e
Kiran Mathew <km468@cornell.edu>
2015-07-24 20:39:39 -0400

    updated configurations.py

--- dev_scripts/configurations.py
+++ dev_scripts/configurations.py
@@ -1,149 +1,132 @@
-#from __future__ import divison, unicode_literals, print_function
+from __future__ import division, unicode_literals, print_function
 
 """
-Compute all configurations of 2D materials on substrates by identifying the Wyckoff positions of the atoms
-To be used after determining the super-lattices from the hetero_inteface.py script
+Compute all configurations of 2D materials on substrates by identifying
+the Wyckoff positions of the atoms.
+To be used after determining the super-lattices from the hetero_inteface.py
+script
 """
 
 import sys 
-from math import sqrt
-from copy import copy
 
 import numpy as np
 
 from pymatgen.symmetry.analyzer import SpacegroupAnalyzer
 from pymatgen.core.structure import Structure
-from pymatgen.core.lattice import Lattice
 
-from mpinterfaces import get_struct_from_mp
-from mpinterfaces.interface import Interface
-from mpinterfaces.transformations import reduced_supercell_vectors
 
-def get_all_config_pos( mat2d,substrate ):
+def get_uniq_layercoords(struct, nlayers, top=True):
+    """
+    returns the coordinates of unique sites in the top or bottom
+    nlayers of the given structure.
 
-	"""
-	Inputs: Lattice-matched and symmetry-matched 2D material and 
-	substrate POSCAR files
-	2D Materials lattice constants adjusted to the substrate lattice constants
-	Output: Returns all possible non-equivalent configurations of 2D material 
-	on substrate by identifying the non-equivalent positions near the interface
-	using spglib
-	Gives additional random placement of 2D material on substrate (to implement)
-	"""
+    Args:
+        struct: input structure
+        nlayers: number of layers
+        top: top or bottom layers, default is top layer
 
-	if mat2d is None and substrate is None:
-		pos_2d=Structure.from_file('POSCAR_2D')
-		pos_sub=Structure.from_file('POSCAR_substrate')
-	else:
-		pos_2d=mat2d
-		pos_sub=substrate
+    Return:
+        numpy array of unique coordinates
+    """
+    coords = np.array([site.coords for site in struct])
+    z = coords[:,2]
+    zu, zuind, = np.unique(z, return_index=True)
+    if top:
+        z_nthlayer = z[zuind[-nlayers]]
+        zfilter = (z >= z_nthlayer)
+    else:
+        z_nthlayer = z[zuind[nlayers - 1]]
+        zfilter = (z <= z_nthlayer)
+    # site indices in the layers        
+    indices_layers = np.argwhere(zfilter).ravel()
+    sa = SpacegroupAnalyzer(struct)
+    symm_data = sa.get_symmetry_dataset()
+    # equivalency mapping for the structure
+    # i'th site in the struct equivalent to eq_struct[i]'th site
+    eq_struct = symm_data["equivalent_atoms"]
+    # equivalency mapping for the layers
+    eq_layers = eq_struct[indices_layers]
+    print(eq_layers)
+    # site indices of unique atoms in the layers
+    __, ueq_layers_indices = np.unique(eq_layers, return_index=True)
+    print(ueq_layers_indices) 
+    indices_uniq = indices_layers[ueq_layers_indices]
+    print(indices_uniq)
+    # coordinates of the unique atoms in the layers
+    print(coords[indices_uniq])
+    return coords[indices_uniq]
+    
 
-	"""
-	Identify the non equivalent atomic positions of the 2 POSCARs
-	Extract the non equivalent atomic positions of top 2 layers of substrate
-	and bottom 2 layers of 2D material 
-	"""
-	finder2d = SpacegroupAnalyzer(pos_2d)
-	finderbulk= SpacegroupAnalyzer(pos_sub) 
-	non_equi_2d=finder2d.get_symmetry_dataset()
-	non_equi_sub=finderbulk.get_symmetry_dataset()
-	set_2d=non_equi_2d["equivalent_atoms"]
-	set_subs=non_equi_sub["equivalent_atoms"]
+def generate_all_configs(mat2d, substrate,
+                         nlayers_2d = 2, nlayers_substrate = 2 ,
+                         seperation = 5 ):
+    """
+    For the given lattice matched 2D material and substrate structures,
+    this functions computes all unique sites in the interface layers
+    and subsequently generates all possible unique 2d/substrate
+    interfaces and writes the corresponding poscar files
 
-	"""
-	Align the POSCAR of 2D material on substrate with all combinations of 
-	non-equilavent atomic locations directly upon each other along with 
-	a random placement of 2D material on substrate
-	specify seperation of the 2D material and substrate in Angstrom
-	"""
-	seperation=5
-	
-	#assuming that the POSCARs are created with z positions in ascending order
-	#identify array number of top two layers of substrate
-	#and bottom 2 layers of 2D material
-	#for each materials, check "equivalent_atoms" array number and delete 
-	#duplicates
-	#save the atomic positions for each material in 2 seperate arrays
-	#shift the atomic positions of the 2D material, by identifying all combinations of
-	#substrate and 2D materials resultant atomic locations
+    Args:
+        mat2d: Lattice and symmetry-matched 2D material structure
+        substrate: Lattice and symmetry-matched 2D substrate structure
+        nlayers_substrate: number of substrate layers
+        nlayers_2d: number of 2d material layers
+        seperation: seperation between the substrate and the 2d
+                    material
+        
+    Returns:
+        None
+        
+    TODO: give additional random placement of 2D material on substrate
+    """
+    # immediate exit if no structures
+    if not(mat2d and substrate):
+        print("no structures. aborting ...")
+        sys.exit()    
+    # unique site coordinates in the substrate top layers
+    coords_uniq_sub = get_uniq_layercoords(substrate,
+                                           nlayers_substrate,
+                                           top=True)
+    # unique site coordinates in the 2D material bottom layers    
+    coords_uniq_2d = get_uniq_layercoords(mat2d,
+                                          nlayers_2d,
+                                          top=False)
+    substrate_top_z = np.max(np.array([site.coords
+                                       for site in substrate])[:,2])
+    mat_2d_bottom = np.min(np.array([site.coords
+                                     for site in mat2d])[:,2])
+    # shift normal to the surface by 'seperation'
+    surface_normal = substrate.lattice.matrix[2,:]
+    origin = np.array([0,0,substrate_top_z])
+    shift_normal = surface_normal/np.linalg.norm(surface_normal) * seperation
 
-	#coordinates of the substrate atoms
-	
-	coor_substrate=(np.array([site.coords for site in pos_sub]))
-	
-	#z coordinate array of the substrate poscar
-	
-	z=coor_substrate[:,2]
-	
-	#array of indices of the top two layers of the substrate poscar
-	
-	indices_top=np.where(z==max(z))
-	indices_top=indices_top[0]
-        np.place(z,z==np.max(z),[-1000])
-        indices_top2=np.where(z==max(z))
-        indices_top=np.concatenate((indices_top, indices_top2[0]),axis=1)
+    #generate all possible interfaces, one for each combination of
+    # unique substrate and unique 2d materials site in the layers .i.e
+    # an interface structure for each parallel shift
+    #interface = 2D material + substrate
+    for i, coord_i in enumerate(coords_uniq_sub):
+        for j, coord_j in enumerate(coords_uniq_2d):
+            interface = substrate.copy()
+            shift_parallel = coord_i - coord_j
+            shift_parallel[2] = 0
+            shift_net = shift_normal - shift_parallel
+            for site in mat2d:
+                new_coords = site.coords
+                new_coords[2] = site.coords[2] - mat_2d_bottom
+                new_coords = new_coords + origin + shift_net
+                interface.append(site.specie, new_coords,
+                                 coords_are_cartesian=True)
+            interface.to(fmt='poscar',
+                         filename='POSCAR_final_'+str(i)+'_'+str(j)+'.vasp')
 
-	#equivalent positions list for the top two layers of atoms in substrate
-	
-	short_set_subs=[set_subs[i] for i in indices_top]
-
-	#all unique atoms in the top two layer of the substrate
-	
-	u,indices_unique=np.unique(short_set_subs,return_index=True)
-	indices_true=[indices_top[i] for i in indices_unique]
-
-	# coordinates of the unique atoms in the top two layers of the substrat
-	
-	coor_unique_sub=coor_substrate[indices_true]
-
-	#coordinates of 2D material atoms
-	
-	coor_2d=(np.array([site.coords for site in pos_2d]))
-
-	#z coordinate array of the 2D material poscar
-	
-	z=coor_2d[:,2]
-
-	#array of indices of bottom two layer of the 2D material poscar
-	
-	indices_bottom=np.where(z==min(z))
-	indices_bottom=indices_bottom[0]
-	np.place(z,z==np.min(z),[1000])
-	indices_bottom2=np.where(z==min(z))
-	indices_bottom=np.concatenate((indices_bottom, indices_bottom2[0]),axis=1)
-	#equivalent positions list for bottom two layers of atoms in 2D material
-	
-	short_set_2d=[set_2d[i] for i in indices_bottom]
-	#all unique atoms in the bottom two layers of the 2D material
-	
-	u,indices_unique=np.unique(short_set_2d,return_index=True)
-	indices_true=[indices_bottom[i] for i in indices_unique]
-
-	# coordinates of the unique atoms in the bottom two layers of the 2D material
-	
-	coor_unique_2d=coor_2d[indices_true]
-
-	#create the POSCARs of 2D material on the substrate
-	
-	for i in range(0,coor_unique_sub.ndim-1) :
-		for j in range(0,coor_unique_2d.ndim) :
-			shift_position=coor_unique_sub[i]-coor_unique_2d[j]
-			shift_position[2]=0
-			substrate_top_z = np.max(np.array([site.coords for site in substrate])[:,2])
-			mat_2d_bottom=np.min(np.array([site.coords for site in mat2d])[:,2])
-			#shift origin and vector
-			shift = substrate.lattice.matrix[2,:]
-			shift = shift/np.linalg.norm(shift) * seperation
-			origin = np.array([0,0,substrate_top_z])
-			for site in mat2d:
-				new_coords=site.coords
-				new_coords[2]=site.coords[2]-mat_2d_bottom
-				new_coords = new_coords + origin  +  shift -shift_position
-				substrate.append(site.specie, new_coords, coords_are_cartesian=True)
-			substrate.to(fmt='poscar', filename='POSCAR_final_'+str(i)+'_'+str(j)+'.vasp')
-"""
-An example
-"""
-pos_2d=Structure.from_file('POSCAR_2D')
-pos_sub=Structure.from_file('POSCAR_substrate')
-a=get_all_config_pos(pos_2d,pos_sub)
+            
+# test
+if __name__ == '__main__':            
+    mat2d = Structure.from_file('POSCAR_2D')
+    substrate = Structure.from_file('POSCAR_substrate')
+    seperation = 5
+    nlayers_2d = 2
+    nlayers_substrate = 2
+    generate_all_configs(mat2d, substrate,
+                       nlayers_2d, nlayers_substrate,
+                       seperation )

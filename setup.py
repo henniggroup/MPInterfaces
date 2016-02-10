ec7d3a5d929faad8f9ecde2e44930180c9cc0d85
Michael Ashton <mashton@ufl.edu>
2016-02-10 12:36:46 -0500

    Use setup.py

--- setup.py
+++ setup.py
@@ -3,13 +3,10 @@ from distutils.core import setup
 
 setup(
   name = 'twod_materials',
-  packages = ['twod_materials'], # this must be the same as the name above
-  version = '0.1',
-  description = 'High throughput',
+  packages = ['twod_materials'],
+  version = '1.0',
+  description = 'High throughput 2D material modules',
   author = 'Michael Ashton',
   author_email = 'mashton@ufl.edu',
-  url = 'https://github.com/ashtonmv/twod_materials', # use the URL to the github repo
-  download_url = 'https://github.com/ashtonmv/twod_materials/tarball/0.1', # I'll explain this in a second
-  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
-  classifiers = [],
+  url = 'https://github.com/ashtonmv/twod_materials',
 )

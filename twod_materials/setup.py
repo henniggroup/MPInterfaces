99b8ca9755da12c4e68649ad34c4064df5f237fd
Joshua Gabriel <joshgabriel92@ufl.edu>
2016-11-18 10:58:13 -0500

    re-introduce conflicting requirements.txt setup.py for further checks, and twod_materials requirements and setup.py

---
+++ twod_materials/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+setup(
+  name = 'twod_materials',
+  packages = find_packages(),
+  version = '1.0',
+  description = 'High throughput 2D material modules',
+  author = 'Michael Ashton',
+  author_email = 'mashton@ufl.edu',
+  url = 'https://github.com/ashtonmv/twod_materials',
+)

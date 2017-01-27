395ce8abeb8b3c63455003cf9b71ada4fb2a7910
josh92 <joshgabriel92@ufl.edu>
2017-01-26 21:54:59 -0500

    add test files for twod_materials.stability

--- twod_materials/stability/tests/test_stability.py
+++ twod_materials/stability/tests/test_stability.py
@@ -2,24 +2,40 @@ import unittest
 
 import os
 
-from twod_materials.stability.analysis import (get_competing_phases,
-                                               get_hull_distances)
+from twod_materials.stability.startup import *
+from twod_materials.stability.analysis import *
+
+import twod_materials
 
 
-ROOT = os.getcwd()
-os.chdir('twod_materials/stability/tests')
+PACKAGE_PATH = twod_materials.__file__.replace('__init__.pyc', '')
+PACKAGE_PATH = PACKAGE_PATH.replace('__init__.py', '')
+ROOT = os.path.join(PACKAGE_PATH, 'stability/tests')
+
+class StartupTest(unittest.TestCase):
+
+    def test_relax_creates_files(self):
+        os.chdir(ROOT)
+        os.chdir('BiTeCl')
+        relax(submit=False, force_overwrite=True)
+        for f in ['KPOINTS', 'INCAR', 'runjob']:
+            self.assertTrue(os.path.isfile(f))
+            os.system('rm {}'.format(f))
 
 
 class AnalysisTest(unittest.TestCase):
 
-    def test_get_hull_distances_for_BiTeCl(self):
-        self.assertEqual(get_hull_distances(['BiTeCl']),
-                         {u'BiTeCl': 0.10335952666666692})
+    def test_get_hull_distance_for_BiTeCl(self):
+        os.chdir(ROOT)
+        os.chdir('BiTeCl')
+        self.assertEqual(get_hull_distance(), 0.10335952666666692)
 
-    def test_get_competing_phases_for_SiP(self):
-        competing_phases = get_competing_phases(['BiTeCl'])
+    def test_get_competing_phases_for_BiTeCl(self):
+        os.chdir(ROOT)
+        os.chdir('BiTeCl')
+        competing_phases = get_competing_phases()
         self.assertEqual(competing_phases, [(u'BiTeCl', u'mp-28944')])
+
 
 if __name__ == '__main__':
     unittest.main()
-    os.chdir(ROOT)

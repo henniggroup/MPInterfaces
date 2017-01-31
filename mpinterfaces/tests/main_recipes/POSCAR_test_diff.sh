8180fe98324348dd73443c8454fb193ce672502c
Seve Monahan <monahan.seve@gmail.com>
2017-01-31 12:25:48 -0500

    Files match working

--- mpinterfaces/tests/main_recipes/POSCAR_test_diff.sh
+++ mpinterfaces/tests/main_recipes/POSCAR_test_diff.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-diff ../../POSCAR_diacetate_boxed.vasp ../../test_files/POSCAR_diacetate_boxed.vasp
+if [[ $(diff ../../POSCAR_diacetate_boxed.vasp \
+      ../../test_files/POSCAR_diacetate_boxed.vasp |\
+       tr -d ' \n\r\t ' | wc -c) -ne 0 ]]; then
+    echo "Error: POSCAR_diacetate_boxed.vasp does not match test file"
+    exit 1
+fi
+    echo "Files match: success"

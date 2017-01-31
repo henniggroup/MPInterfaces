4c955ae91dfe7d5adce75e06b7a5d592eef59325
Seve Monahan <monahan.seve@gmail.com>
2017-01-31 12:37:54 -0500

    Test all files generated by python interface.py

--- mpinterfaces/tests/main_recipes/POSCAR_test_diff.sh
+++ mpinterfaces/tests/main_recipes/POSCAR_test_diff.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-if [[ $(diff ../../POSCAR_diacetate_boxed.vasp \
-      ../../test_files/POSCAR_diacetate_boxed.vasp |\
-       tr -d ' \n\r\t ' | wc -c) -ne 0 ]]; then
-    echo "Error: POSCAR_diacetate_boxed.vasp does not match test file"
-    exit 1
-fi
-    echo "Files match: success"
+File_differs_from_test(){
+    echo "Testing $1"
+    if [[ $(diff ../../$1 \
+          ../../test_files/$1 |\
+           tr -d ' \n\r\t ' | wc -c) -ne 0 ]]; then
+        echo "Error: $1 does not match test file"
+        exit 1
+    fi
+        echo "Files match: success"
+}
+
+File_differs_from_test "POSCAR_diacetate_boxed.vasp"
+File_differs_from_test "POSCAR_interface.vasp"
+File_differs_from_test "POSCAR_slab.vasp"

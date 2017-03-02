5782fdaf02b08af9e948632eb093a6ec80016d04
Michael Ashton <mashton@ufl.edu>
2017-03-02 15:37:51 -0500

    Use matplotlib data for tests, not file creation.

--- twod_materials/electronic_structure/tests/test_electronic_structure.py
+++ twod_materials/electronic_structure/tests/test_electronic_structure.py
@@ -12,6 +12,7 @@ PACKAGE_PATH = twod_materials.__file__.replace('__init__.pyc', '')
 PACKAGE_PATH = PACKAGE_PATH.replace('__init__.py', '')
 ROOT = os.path.join(PACKAGE_PATH, 'electronic_structure/tests')
 
+
 class StartupTest(unittest.TestCase):
 
     def test_run_pbe_calculation_creates_files(self):
@@ -55,6 +56,7 @@ class AnalysisTest(unittest.TestCase):
         for edge in test_edges:
             self.assertAlmostEqual(test_edges[edge], control_edges[edge])
 
+
     def test_get_fermi_velocities_for_MoS2_and_FeCl2(self):
         os.chdir(ROOT)
         os.chdir('MoS2')
@@ -72,54 +74,84 @@ class AnalysisTest(unittest.TestCase):
         for i in range(len(test_velocities)):
             self.assertEqual(test_velocities[i], control_velocities[i])
 
-    """
-    def test_plot_band_alignments_creates_file(self):
+
+    def test_plot_band_alignments_creates_data(self):
         os.chdir(ROOT)
-        plot_band_alignments(['MoS2'], fmt='png', latex=False)
-        self.assertTrue(os.path.isfile('band_alignments.png'))
-        os.system('rm band_alignments.png')
+        ax = plot_band_alignments(['MoS2'], fmt='None')
+        self.assertEqual(ax.get_children()[0].get_height(), 1.23)
 
 
-    def test_plot_local_potential_creates_file(self):
+    def test_plot_local_potential_creates_data(self):
         os.chdir(ROOT)
         os.chdir('MoS2')
-        plot_local_potential(fmt='png', latex=False)
-        self.assertTrue(os.path.isfile('locpot.png'))
-        os.system('rm locpot.png')
+        ax = plot_local_potential(fmt='None')
+        test_line = ax.get_lines()[0]
+        test_x, test_y = test_line.get_xdata(), test_line.get_ydata()
+        control_lines = open('locpot_data.txt').readlines()
+        control_x = [float(d) for d in control_lines[0].split(',')]
+        control_y = [float(d) for d in control_lines[1].split(',')]
+        for i in range(len(control_x)):
+            self.assertAlmostEqual(test_x[i], control_x[i])
+            self.assertAlmostEqual(test_y[i], control_y[i])
 
 
-    def test_plot_band_structure_creates_file(self):
+    def test_plot_band_structure_creates_data(self):
         os.chdir(ROOT)
         os.chdir('band_structure_control')
-        plot_band_structure(fmt='png', latex=False)
-        self.assertTrue(os.path.isfile('band_structure.png'))
-        os.system('rm band_structure.png')
+        ax = plot_band_structure(fmt='None')
+        test_line = ax.get_lines()[0]
+        test_x, test_y = test_line.get_xdata(), test_line.get_ydata()
+        control_x = [
+            0.0, 0.07230748, 0.14461496, 0.21692245, 0.28922993, 0.36153741,
+            0.43384489, 0.50615237, 0.57845986, 0.65076734, 0.72307482,
+            0.7953823 , 0.86768978, 0.93999727, 1.01230475, 1.08461223,
+            1.15691971, 1.22922719, 1.30153468, 1.37384216, 1.44614964
+        ]
+        control_y = [
+            -19.4417, -19.4287, -19.3895, -19.3245, -19.2338, -19.1181,
+            -18.9778, -18.8139, -18.6275, -18.4202, -18.1937, -17.9507,
+            -17.6945, -17.4296, -17.162 , -16.8998, -16.6534, -16.4361,
+            -16.2634, -16.1514, -16.1125
+        ]
+        for i in range(len(control_x)):
+            self.assertAlmostEqual(test_x[i], control_x[i])
+            self.assertAlmostEqual(test_y[i], control_y[i])
 
 
     def test_plot_color_projected_bands_creates_file(self):
         os.chdir(ROOT)
         os.chdir('band_structure_control')
-        plot_color_projected_bands(fmt='png', latex=False)
-        self.assertTrue(os.path.isfile('color_projected_bands.png'))
-        os.system('rm color_projected_bands.png')
+        ax = plot_color_projected_bands(fmt='None')
+        test_line = ax.get_lines()[6]
+        test_x, test_y = test_line.get_xdata(), test_line.get_ydata()
+        control_x, control_y = [0.14461496, 0.21692245], [-19.3895, -19.3245]
+        for i in range(len(control_x)):
+            self.assertAlmostEqual(test_x[i], control_x[i])
+            self.assertAlmostEqual(test_y[i], control_y[i])
 
 
-    def test_plot_elt_projected_bands_creates_file(self):
+    def test_plot_elt_projected_bands_creates_data(self):
         os.chdir(ROOT)
         os.chdir('band_structure_control')
-        plot_elt_projected_bands(fmt='png', latex=False)
-        self.assertTrue(os.path.isfile('elt_projected_bands.png'))
-        os.system('rm elt_projected_bands.png')
+        ax = plot_elt_projected_bands(fmt='None')
+        test_line = ax.get_lines()[7]
+        test_x, test_y = test_line.get_xdata()[0], test_line.get_ydata()[0]
+        control_x, control_y = 0.14461496, -19.3895
+        self.assertAlmostEqual(test_x, control_x)
+        self.assertAlmostEqual(test_y, control_y)
 
 
-    def test_plot_orb_projected_bands_creates_file(self):
+    def test_plot_orb_projected_bands_creates_data(self):
         os.chdir(ROOT)
         os.chdir('band_structure_control')
         orbitals = {'B': ['s', 'p'], 'N': ['s', 'p']}
-        plot_orb_projected_bands(orbitals, fmt='png', latex=False)
-        self.assertTrue(os.path.isfile('orb_projected_bands.png'))
-        os.system('rm orb_projected_bands.png')
-    """
+        ax = plot_orb_projected_bands(orbitals, fmt='None')
+        test_line = ax.get_lines()[8]
+        test_x, test_y = test_line.get_xdata()[0], test_line.get_ydata()[0]
+        control_x, control_y = 0.21692245, -19.3245
+        self.assertAlmostEqual(test_x, control_x)
+        self.assertAlmostEqual(test_y, control_y)
+
 
 if __name__ == '__main__':
     unittest.main()

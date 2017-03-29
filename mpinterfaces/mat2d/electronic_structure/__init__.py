18f284cd7dfddb6f761d6a19f31746880400905e
Michael Ashton <mashton@mseadmins-iMac.local>
2017-03-29 17:22:22 -0400

    Fix import statement.

--- mpinterfaces/mat2d/electronic_structure/__init__.py
+++ mpinterfaces/mat2d/electronic_structure/__init__.py
@@ -1,4 +1,3 @@
-from mpinterfaces.mat2d.electronic_structure.startup import get_markovian_path, \
+from mpinterfaces.mat2d.electronic_structure.startup import \
     get_2D_hse_kpoints, get_2D_incar_hse, get_2D_incar_hse_prep, \
-    run_hse_calculation, run_hse_prep_calculation, run_pbe_calculation, \
-    remove_z_kpoints_linemode
+    run_hse_calculation, run_hse_prep_calculation, run_pbe_calculation

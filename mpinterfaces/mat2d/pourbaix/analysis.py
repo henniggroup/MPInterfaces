fd2f6eb943c4a62380bd359d90934b0e27bdfeb4
Michael Ashton <ashtonmv@gmail.com>
2017-09-27 17:21:19 -0400

    Fractional comp_dict values.

--- mpinterfaces/mat2d/pourbaix/analysis.py
+++ mpinterfaces/mat2d/pourbaix/analysis.py
@@ -140,7 +140,8 @@ def plot_pourbaix_diagram(metastability=0.0, ion_concentration=1e-6, fmt='pdf'):
 
     all_entries = [pbx_cmpd] + pbx_ion_entries
 
-    comp_dict = {el.symbol: composition[el] for el in composition}
+    total = sum([composition[el] for el in composition])
+    comp_dict = {el.symbol: composition[el]/total for el in composition}
     pourbaix_diagram = PourbaixDiagram(all_entries, comp_dict=comp_dict)
 
     plotter = PourbaixPlotter(pourbaix_diagram)

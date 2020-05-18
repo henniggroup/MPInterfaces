cd32b4e6416e287adc359af86f4ede95daf29253
Chaitanya Kolluru <kvs.chaitanya@ufl.edu>
2020-05-18 18:49:54 -0400

    changes to get_trans_matrices and new functionalities; generates multiple lattice matches; example eg_lma.py

---
+++ mpinterfaces/tests/eg_lma.py
@@ -0,0 +1,42 @@
+# coding: utf-8
+# Copywrite (c) Henniggroup.
+# Distributed under the terms of the MIT License.
+
+from __future__ import division, unicode_literals, print_function
+
+"""
+Utility script to find if endpoint slabs are matching with substrate
+
+Usage: Provide inputs <path to poscar substrate>, <path to poscar 2D> and
+       match constraints using for the search
+
+       python get_matching_matrix.py
+"""
+# import the lma module
+from mpinterfaces import transformations
+from pymatgen import Structure, Lattice
+import numpy as np
+
+#### Inputs for lattice matching #####
+# substrate file path
+sub_file = 'POSCAR_sub'
+# twod file path
+twod_file = 'POSCAR_twod'
+sub = Structure.from_file(sub_file)
+twod = Structure.from_file(twod_file)
+
+# provide input match constraints as a dictionary
+match_constraints = {'max_area':90, 'max_mismatch':0.06, 'max_angle_diff':2,
+                     'r1r2_tol':0.04, 'separation': 2.2, 'nlayers_substrate':1,
+                     'nlayers_2d':1, 'sd_layers':0, 'best_match': 'area'}
+#########################################
+
+# Lowest area (default) or minimum uv mismatched lattice match
+# is returned based on 'best_match' option in match_constraints
+# best_match should be either 'area' or 'mismatch'
+iface, n_sub, z_ub = lma.run_lat_match(sub, twod, match_constraints)
+
+if iface is None:
+    print ('Current twod lattice matches with substrate. No changes needed!')
+else:
+    iface.to(filename='POSCAR_iface', fmt='poscar')
